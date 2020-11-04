fe4b0195a1dae5ef27aebe64bd7d6173e1b9d528
droll-framework-1384 <ebyan.yusuf@gmail.com>
2020-11-04 17:44:04 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,16 @@
+def meal_choice(veg1, veg2, protein = 'tofu')
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+
+def meal_choice(veg1, veg2, protein = 'tofu')
+  puts "What a nutritious meal!"
+  meal =  "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts meal
+  meal
+end
+
+
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
