2a84b6ec9c086a2e5fe5cead30fc52b309307bb3
droll-framework-1384 <ebyan.yusuf@gmail.com>
2020-11-04 17:41:27 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,7 @@
 def meal_choice(veg1, veg2, protein)
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
 def meal_choice(veg1, veg2, protein)
